9dc0ba681a3e7a5be8a25d7ee6718390aea28fdc
Neha Ann <nehabenno06@gmail.com>
2025-12-10 21:46:35 +0530

    Pencil sketch project draft

--- src/main.py
+++ src/main.py
@@ -1,21 +1,25 @@
 import cv2
-path=r"C:\Users\neha0\Downloads\Passport size Neha.jpg"
+path=r"C:\Users\neha0\OneDrive\Desktop\builder cv\task1\tom.webp"
 image=cv2.imread(path)
 imginvert=255-image
 
 imagegray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 invertgray=255-imagegray
-gaussianblur=cv2.GaussianBlur(invertgray,(91,91),0)
+gaussianblur=cv2.GaussianBlur(invertgray,(81,81),0)
 invertgaussianblur=255-gaussianblur
 sketch=cv2.divide(imagegray,invertgaussianblur,scale=265.0)
 
 cv2.imshow("Image",image)
-cv2.imshow("invert",imginvert)
-cv2.imshow("gray",imagegray)
-cv2.imshow("invertgray",invertgray)
-cv2.imshow("gaussianblur",gaussianblur)
-cv2.imshow("invertgaussianblur",invertgaussianblur)
+#cv2.imshow("invert",imginvert)
+#cv2.imshow("gray",imagegray)
+#cv2.imshow("invertgray",invertgray)
+#cv2.imshow("gaussianblur",gaussianblur)
+#cv2.imshow("invertgaussianblur",invertgaussianblur)
 cv2.imshow("sketch",sketch)
+
+outputpath = r"C:\Users\neha0\OneDrive\Desktop\builder cv\task1\Build-Club-Recruitment\results\sketchoutput.png"
+cv2.imwrite(outputpath, sketch)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
